1eb5f9b2b96f4ebf5f25b26b0b7d70169b0d3389
Karpushin Vsevolod <sivass3535@gmail.com>
2022-12-16 17:24:07 +0300

    feat: add kotlin-part-1 training

--- app/src/main/java/com/example/repo/MainActivity.kt
+++ app/src/main/java/com/example/repo/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.repo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.repo.kotlin.KotlinPart1
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val kotlinPart1 = KotlinPart1()
+
+        //3.
+        val book3 = KotlinPart1.Book(BigDecimal(15.5), 7500)
+        val book4 = KotlinPart1.Book(BigDecimal(5.9), 1000)
+        val magazine = KotlinPart1.Magazine(BigDecimal(1.56), 300)
+
+        kotlinPart1.printInfo(book3)
+        kotlinPart1.printInfo(book4)
+        kotlinPart1.printInfo(magazine)
+
+        kotlinPart1.printCompareResults(book3, book4)
+
+        //4.
+        val book1: KotlinPart1.Book? = null
+        val book2 = KotlinPart1.Book(BigDecimal(56.9), 1000)
+
+        book1?.let {
+            kotlinPart1.buy(it)
+        }
+        book2.let {
+            kotlinPart1.buy(it)
+        }
+
+        //5.
+        kotlinPart1.sum.invoke(13, 6)
     }
 }
