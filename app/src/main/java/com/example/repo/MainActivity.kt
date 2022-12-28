1b91c8645a12a0d3fef74ea51482ac546733110e
Karpushin Vsevolod <sivass3535@gmail.com>
2022-12-28 17:38:21 +0300

    feat: add biding, switching to a profile fragment

--- app/src/main/java/com/example/repo/MainActivity.kt
+++ app/src/main/java/com/example/repo/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.repo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.example.repo.databinding.ActivityMainBinding
+import com.example.repo.ui.screen.ProfileFragment
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navView.menu.findItem((R.id.navigationAccount)).isChecked = true
+        supportFragmentManager.commit {
+            replace(
+                R.id.screenContainer,
+                ProfileFragment.newInstance()
+            )
+            setReorderingAllowed(true)
+        }
     }
 }
