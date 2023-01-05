b95f3013efed84ee5b68371fb4b6d69f6dd5b674
Karpushin Vsevolod <sivass3535@gmail.com>
2023-01-05 19:33:23 +0300

    feat: update navigation

--- app/src/main/java/com/example/repo/MainActivity.kt
+++ app/src/main/java/com/example/repo/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.repo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.repo.databinding.ActivityMainBinding
+import com.example.repo.ui.screen.CategoriesOfHelpingFragment
 import com.example.repo.ui.screen.ProfileFragment
+import com.example.repo.ui.screen.SearchFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +17,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.navView.menu.findItem((R.id.navigationAccount)).isChecked = true
+        binding.navView.menu.findItem((R.id.navigationHelp)).isChecked = true
+        loadFragment(CategoriesOfHelpingFragment.newInstance())
+
+        binding.navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigationHelp -> {
+                    loadFragment(CategoriesOfHelpingFragment.newInstance())
+
+                    true
+                }
+                R.id.navigationAccount -> {
+                    loadFragment(ProfileFragment.newInstance())
+
+                    true
+                }
+                R.id.navigationSearch -> {
+                    loadFragment(SearchFragment.newInstance())
+
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
         supportFragmentManager.commit {
             replace(
                 R.id.screenContainer,
-                ProfileFragment.newInstance()
+                fragment
             )
-            setReorderingAllowed(true)
         }
     }
 }
