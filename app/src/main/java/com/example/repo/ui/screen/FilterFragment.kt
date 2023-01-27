13e6a65fe223d94eec2dc1339bf94dd55e3a0e93
Karpushin Vsevolod <sivass3535@gmail.com>
2023-01-27 17:06:02 +0300

    refactor: use delegate to initialize viewModel, set layout manager of recycler in xml

--- app/src/main/java/com/example/repo/ui/screen/FilterFragment.kt
+++ app/src/main/java/com/example/repo/ui/screen/FilterFragment.kt
@@ -10,9 +10,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.repo.ExampleService
 import com.example.repo.ExampleService.Companion.ACTION_MY_INTENT_SERVICE
 import com.example.repo.R
@@ -29,7 +28,7 @@ class FilterFragment : Fragment() {
     private lateinit var binding: FragmentFilterBinding
     private lateinit var dataProvider: DataProvider
     private val filterAdapter by lazy { FilterAdapter() }
-    private lateinit var viewModel: FilterViewModel
+    private val viewModel by activityViewModels<FilterViewModel>()
     private var broadcastReceiver: BroadcastReceiver
     private var filter = IntentFilter(ACTION_MY_INTENT_SERVICE)
 
@@ -44,7 +43,6 @@ class FilterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewModel = ViewModelProvider(requireActivity())[FilterViewModel::class.java]
         dataProvider = DataProvider(requireContext())
         binding = FragmentFilterBinding.inflate(inflater)
         return binding.root
@@ -60,7 +58,6 @@ class FilterFragment : Fragment() {
             }
         } else {
             binding.filterRV.apply {
-                layoutManager = LinearLayoutManager(requireContext())
                 adapter = filterAdapter.apply {
                     filters = savedFilters!!
                 }
@@ -119,7 +116,6 @@ class FilterFragment : Fragment() {
                 viewModel.savedFilters ?: Gson().fromJson(json, FilterList::class.java).filters
             binding.progressBar.visibility = View.GONE
             binding.filterRV.apply {
-                layoutManager = LinearLayoutManager(requireContext())
                 adapter = filterAdapter.apply {
                     filters = savedFilters!!
                 }
