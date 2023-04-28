5e3bb4b8aeb89418901be4f8230566d45b4add57
Karpushin Vsevolod <vsev.karp@mail.ru>
2023-04-28 16:31:33 +0300

    fix: remove not necessary LazyColumn

--- auth-feature/src/main/java/com/example/auth_feature/presentation/views/AuthScreen.kt
+++ auth-feature/src/main/java/com/example/auth_feature/presentation/views/AuthScreen.kt
@@ -3,8 +3,9 @@ package com.example.auth_feature.presentation.views
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -62,218 +63,217 @@ fun AuthScreen(
         },
         containerColor = AppTheme.colors.white
     ) { contentPadding ->
-        LazyColumn(Modifier.padding(contentPadding)) {
-            item {
-                Column(
-                    Modifier.padding(horizontal = dimensionResource(id = R.dimen.spacing_l))
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.auth_invite_text),
-                        modifier = Modifier
-                            .padding(
-                                start = 0.dp,
-                                top = dimensionResource(id = R.dimen.auth_invite_margin_top),
-                                end = 0.dp,
-                                bottom = 0.dp
-                            )
-                            .align(Alignment.CenterHorizontally),
-                        fontSize = 14.sp,
-                        color = AppTheme.colors.black70,
-                        textAlign = TextAlign.Center
+        Column(
+            Modifier
+                .padding(contentPadding)
+                .padding(horizontal = dimensionResource(id = R.dimen.spacing_l))
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = stringResource(id = R.string.auth_invite_text),
+                modifier = Modifier
+                    .padding(
+                        start = 0.dp,
+                        top = dimensionResource(id = R.dimen.auth_invite_margin_top),
+                        end = 0.dp,
+                        bottom = 0.dp
                     )
+                    .align(Alignment.CenterHorizontally),
+                fontSize = 14.sp,
+                color = AppTheme.colors.black70,
+                textAlign = TextAlign.Center
+            )
 
-                    Row(
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .padding(
-                                start = 0.dp,
-                                top = dimensionResource(id = R.dimen.spacing_l),
-                                end = 0.dp,
-                                bottom = 0.dp
-                            )
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_vk),
-                            contentDescription = stringResource(
-                                id = R.string.vk_logo_description
-                            )
-                        )
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_fb),
-                            contentDescription = stringResource(
-                                id = R.string.fb_logo_description
-                            ),
-                            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.auth_invite_margin_top))
-                        )
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_ok),
-                            contentDescription = stringResource(
-                                id = R.string.ok_logo_description
-                            )
-                        )
-                    }
-
-                    Text(
-                        text = stringResource(id = R.string.auth_invite_text2), modifier = Modifier
-                            .padding(
-                                start = 0.dp,
-                                top = dimensionResource(id = R.dimen.auth_invite_margin_top),
-                                end = 0.dp,
-                                bottom = 0.dp
-                            )
-                            .align(Alignment.CenterHorizontally),
-                        fontSize = 14.sp,
-                        color = AppTheme.colors.black70
+            Row(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(
+                        start = 0.dp,
+                        top = dimensionResource(id = R.dimen.spacing_l),
+                        end = 0.dp,
+                        bottom = 0.dp
                     )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_vk),
+                    contentDescription = stringResource(
+                        id = R.string.vk_logo_description
+                    )
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.ic_fb),
+                    contentDescription = stringResource(
+                        id = R.string.fb_logo_description
+                    ),
+                    modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.auth_invite_margin_top))
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.ic_ok),
+                    contentDescription = stringResource(
+                        id = R.string.ok_logo_description
+                    )
+                )
+            }
 
+            Text(
+                text = stringResource(id = R.string.auth_invite_text2), modifier = Modifier
+                    .padding(
+                        start = 0.dp,
+                        top = dimensionResource(id = R.dimen.auth_invite_margin_top),
+                        end = 0.dp,
+                        bottom = 0.dp
+                    )
+                    .align(Alignment.CenterHorizontally),
+                fontSize = 14.sp,
+                color = AppTheme.colors.black70
+            )
+
+            Text(
+                text = stringResource(id = R.string.email_label),
+                modifier = Modifier
+                    .padding(
+                        start = 0.dp,
+                        top = dimensionResource(id = R.dimen.spacing_l),
+                        end = 0.dp,
+                        bottom = 0.dp
+                    ),
+                fontSize = 12.sp,
+                color = AppTheme.colors.black38
+            )
+
+            TextField(
+                value = viewState.value.email,
+                placeholder = {
                     Text(
-                        text = stringResource(id = R.string.email_label),
-                        modifier = Modifier
-                            .padding(
-                                start = 0.dp,
-                                top = dimensionResource(id = R.dimen.spacing_l),
-                                end = 0.dp,
-                                bottom = 0.dp
-                            ),
-                        fontSize = 12.sp,
+                        text = stringResource(id = R.string.email_edit_text_hint),
+                        fontSize = 16.sp,
                         color = AppTheme.colors.black38
                     )
+                },
 
-                    TextField(
-                        value = viewState.value.email,
-                        placeholder = {
-                            Text(
-                                text = stringResource(id = R.string.email_edit_text_hint),
-                                fontSize = 16.sp,
-                                color = AppTheme.colors.black38
-                            )
-                        },
-
-                        onValueChange = {
-                            viewModel.obtainIntent(
-                                AuthIntent.EmailChangedIntent(
-                                    email = it
-                                )
-                            )
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = Color.Transparent,
-                            cursorColor = AppTheme.colors.lightGreen,
-                            focusedIndicatorColor = AppTheme.colors.lightGreen,
-                            unfocusedIndicatorColor = AppTheme.colors.black12
+                onValueChange = {
+                    viewModel.obtainIntent(
+                        AuthIntent.EmailChangedIntent(
+                            email = it
                         )
                     )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    cursorColor = AppTheme.colors.lightGreen,
+                    focusedIndicatorColor = AppTheme.colors.lightGreen,
+                    unfocusedIndicatorColor = AppTheme.colors.black12
+                )
+            )
 
+            Text(
+                text = stringResource(id = R.string.password_label),
+                modifier = Modifier
+                    .padding(
+                        start = 0.dp,
+                        top = dimensionResource(id = R.dimen.spacing_l),
+                        end = 0.dp,
+                        bottom = 0.dp
+                    ),
+                fontSize = 12.sp,
+                color = AppTheme.colors.black38
+            )
+
+            var showPassword by remember { mutableStateOf(false) }
+            TextField(
+                value = viewState.value.password,
+                placeholder = {
                     Text(
-                        text = stringResource(id = R.string.password_label),
-                        modifier = Modifier
-                            .padding(
-                                start = 0.dp,
-                                top = dimensionResource(id = R.dimen.spacing_l),
-                                end = 0.dp,
-                                bottom = 0.dp
-                            ),
-                        fontSize = 12.sp,
+                        text = stringResource(id = R.string.password_edit_text_hint),
+                        fontSize = 16.sp,
                         color = AppTheme.colors.black38
                     )
-
-                    var showPassword by remember { mutableStateOf(false) }
-                    TextField(
-                        value = viewState.value.password,
-                        placeholder = {
-                            Text(
-                                text = stringResource(id = R.string.password_edit_text_hint),
-                                fontSize = 16.sp,
-                                color = AppTheme.colors.black38
-                            )
-                        },
-                        onValueChange = {
-                            viewModel.obtainIntent(
-                                AuthIntent.PasswordChangedIntent(
-                                    password = it
-                                )
-                            )
-                        },
-                        visualTransformation = if (showPassword) {
-                            VisualTransformation.None
-                        } else {
-                            PasswordVisualTransformation()
-                        },
-                        trailingIcon = {
-                            if (showPassword) {
-                                IconButton(onClick = { showPassword = false }) {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_open),
-                                        contentDescription = null
-                                    )
-                                }
-                            } else {
-                                IconButton(onClick = { showPassword = true }) {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_close),
-                                        contentDescription = null
-                                    )
-                                }
-                            }
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = Color.Transparent,
-                            cursorColor = AppTheme.colors.lightGreen,
-                            focusedIndicatorColor = AppTheme.colors.lightGreen,
-                            unfocusedIndicatorColor = AppTheme.colors.black12
+                },
+                onValueChange = {
+                    viewModel.obtainIntent(
+                        AuthIntent.PasswordChangedIntent(
+                            password = it
                         )
                     )
-
-                    Button(
-                        onClick = {
-                            viewModel.obtainIntent(AuthIntent.LoginIntent)
-                        },
-                        modifier = Modifier
-                            .padding(
-                                start = 0.dp,
-                                top = dimensionResource(id = R.dimen.auth_btn_top_margin),
-                                end = 0.dp,
-                                bottom = 0.dp
+                },
+                visualTransformation = if (showPassword) {
+                    VisualTransformation.None
+                } else {
+                    PasswordVisualTransformation()
+                },
+                trailingIcon = {
+                    if (showPassword) {
+                        IconButton(onClick = { showPassword = false }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_open),
+                                contentDescription = null
                             )
-                            .fillMaxWidth(),
-                        enabled = viewState.value.isValid,
-                        shape = RoundedCornerShape(2.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = AppTheme.colors.lightGreen,
-                            contentColor = AppTheme.colors.white,
-                        )
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.auth_btn_text).uppercase(),
-                            style = AppTheme.typography.textStyle17
-                        )
-                    }
-
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier
-                            .padding(
-                                start = 0.dp,
-                                top = dimensionResource(id = R.dimen.spacing_l),
-                                end = 0.dp,
-                                bottom = 0.dp
+                        }
+                    } else {
+                        IconButton(onClick = { showPassword = true }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_close),
+                                contentDescription = null
                             )
-                            .fillMaxWidth()
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.forgot_password_btn_text),
-                            color = AppTheme.colors.lightGreen,
-                            fontSize = 14.sp
-                        )
-                        Text(
-                            text = stringResource(id = R.string.register_btn_text),
-                            color = AppTheme.colors.lightGreen,
-                            fontSize = 14.sp
-                        )
+                        }
                     }
-                }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    cursorColor = AppTheme.colors.lightGreen,
+                    focusedIndicatorColor = AppTheme.colors.lightGreen,
+                    unfocusedIndicatorColor = AppTheme.colors.black12
+                )
+            )
+
+            Button(
+                onClick = {
+                    viewModel.obtainIntent(AuthIntent.LoginIntent)
+                },
+                modifier = Modifier
+                    .padding(
+                        start = 0.dp,
+                        top = dimensionResource(id = R.dimen.auth_btn_top_margin),
+                        end = 0.dp,
+                        bottom = 0.dp
+                    )
+                    .fillMaxWidth(),
+                enabled = viewState.value.isValid,
+                shape = RoundedCornerShape(2.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AppTheme.colors.lightGreen,
+                    contentColor = AppTheme.colors.white,
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.auth_btn_text).uppercase(),
+                    style = AppTheme.typography.textStyle17
+                )
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(
+                        start = 0.dp,
+                        top = dimensionResource(id = R.dimen.spacing_l),
+                        end = 0.dp,
+                        bottom = 0.dp
+                    )
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.forgot_password_btn_text),
+                    color = AppTheme.colors.lightGreen,
+                    fontSize = 14.sp
+                )
+                Text(
+                    text = stringResource(id = R.string.register_btn_text),
+                    color = AppTheme.colors.lightGreen,
+                    fontSize = 14.sp
+                )
             }
         }
     }
